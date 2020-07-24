edbb3639b6c07915cf7b14dd7dfd7911e5e5748b
杨引 <yangyinyin@yuelvhui.com>
2020-07-24 14:34:03 +0800

    update

--- YNetworking/YNetworkRequestBase.h
+++ YNetworking/YNetworkRequestBase.h
@@ -6,17 +6,17 @@
 //  Copyright © 2018  All rights reserved.
 //
 
-#import "YNetworking/YNetworkRequest.h"
+
+#import "YNetworkRequest.h"
 
 /** 接口前缀-开发服务器*/
 UIKIT_EXTERN NSString *const kBaseUrl;
 
-
-
 @interface YNetworkRequestBase : NSObject
 
-
+// denglu
 +(void)requestUrl:(NSString *)url requestMethod:(YRequestType)method param:(NSDictionary *)aParam andhandler:(YHandlerBlock)handlerBlock;
+
 
 @end
 
